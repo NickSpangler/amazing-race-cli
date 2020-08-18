f983e6af7190c79421d38d2f8bcc4aa1878a3a39
NickSpangler <Spangleyman@mac.com>
2020-08-18 09:37:36 -0400

    added css selectors for team links

--- lib/season.rb
+++ lib/season.rb
@@ -28,7 +28,19 @@ end
 #use .each for 11 team names
 #@teams name text = doc.css("font")[0..10].text
 #for below, every odd number before "attr" from 1 (nick and starr) to 21 (anita and arthur) // and need to add "amazingrace.fandom" to beginning of "/wiki/Nick_%26_Starr"
-#@teams link = doc.css("table a")[1].attr("href")
+#@teams link = team_links = [
+#   doc.css("table a")[1].attr("href"),
+#   doc.css("table a")[3].attr("href"),
+#   doc.css("table a")[5].attr("href"),
+#   doc.css("table a")[7].attr("href"),
+#   doc.css("table a")[9].attr("href"),
+#   doc.css("table a")[11].attr("href"),
+#   doc.css("table a")[13].attr("href"),
+#   doc.css("table a")[15].attr("href"),
+#   doc.css("table a")[17].attr("href"),
+#   doc.css("table a")[19].attr("href"),
+#   doc.css("table a")[21].attr("href")
+# ]
 
 # EPISODES
 #@first episode name = doc.css("table")[1].css("tr")[1].children[2].children.children.text
