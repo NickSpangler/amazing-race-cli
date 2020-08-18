1132477f5bd6c4b207231d529f0976960d8f4f2c
NickSpangler <Spangleyman@mac.com>
2020-08-18 09:56:17 -0400

    building teams array in Scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,4 +6,22 @@ class Scraper
         @episodes = []
 
     end
+
+    def self.scrape_teams_from_season(season_page_url)
+        doc = Nokogiri::HTML(open(season_page_url))
+        teams_array = []
+        name_counter = 0
+        link_counter = 1
+        while name_counter <= 10 && link_counter <= 21
+            team = {
+                :name=> doc.css("font")[name_counter].text,
+                :profile_link=> doc.css("table a")[link_counter].attr("href")
+            }
+            teams_array << team
+            name_counter += 1
+            link_counter += 2
+        end
+        teams_array
+    end
+
 end
