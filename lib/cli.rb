c19a1a0ea656687b0418c30efc699b12d10e7913
NickSpangler <Spangleyman@mac.com>
2020-08-19 16:24:22 -0400

    finished 'more_info' method

--- lib/cli.rb
+++ lib/cli.rb
@@ -44,6 +44,19 @@ class CLI
         puts ""
         puts t.about
         puts ""
+        puts "For more info about #{t.name}, enter 'More'."
+        puts "To choose another team, enter 'Teams'."
+        puts "To choose an episode to explore, enter 'Episodes'."
+        input = gets.strip.downcase
+        if input == "more"
+            more_info(t)
+        elsif input == "teams"
+            list_teams
+        elsif input == "episodes"
+            list_episodes
+        else
+            display_team(number)
+        end
         
     end
 
@@ -98,61 +111,25 @@ class CLI
             list_episodes
         end
     end
-    
-    #   def choose_season
-    #     puts "Please select a season of The Amazing Race to explore. You can type a number between 1 and 31."
-    
-    #     input = gets.strip
-    
-    #     display_season(input)
-    #   end
-    
-    #   def display_season(input)
-    #     teams = ["Nick & Starr", "Ken & Tina", "Toni & Dallas", "Dan & Andrew"]
-    #     episodes = ["Episode 1", "Episode 2", "Episode 3", "Episode 4"]
-    
-    #     puts "Here are the teams and episodes from Seaon #{input}."
-    #     teams.each.with_index(1){|team, index| puts "#{index}. #{team}"}
-    #     episodes.each{|episode| puts episode}
-    #     puts "Select a team or episode to read more about it. Or enter 'new season' to choose a new season."
-    #     puts "Or enter 'exit' to exit the program. "
-    
-    #     input = gets.strip.downcase
-    #     case input
-    #       when "new season"
-    #         choose_season
-    #       when "exit"
-    #         exit_program
-    #       else
-    #         display_team_or_episode(input)
-    #     end
-    #   end
-    
-    #   def display_team_or_episode(input)
-    #     puts "Here's everything you want to know about team/episode #{input}."
-    #     puts "What would you like to do next? To list teams and episodes from this season, enter the season number."
-    #     puts "To choose a different season, enter 'new season'."
-    #     puts "To run the race as this team, enter 'Race!'"
-    #     puts "To exit, enter 'exit'."
-    #     input = gets.strip.downcase
-    #     case input
-    #       when "new season"
-    #         choose_season
-    #       when "race!"
-    #         run_race
-    #       when "exit"
-    #         exit_program
-    #       else
-    #         display_season(input)
-    #     end
-    #   end
-    
-    #   def run_race
-    #     puts "Figure out how to make this feature work."
-    #     exit_program
-    #   end
-    
-    #   def exit_program
-    #     puts "Thanks for Racing!"
-    #   end
+
+    def more_info(team)
+        puts ""
+        puts "Profile: #{team.profile}"
+        puts ""
+        puts "Post Race: #{team.post_race}"
+        puts ""
+        puts "Trivia: #{team.trivia}"
+        puts ""
+        puts "To choose another team, enter 'Teams'."
+        puts "To choose an episode to explore, enter 'Episodes'."
+        input = gets.strip.downcase
+        if input == "teams"
+            list_teams
+        elsif input == "episodes"
+            list_episodes
+        else
+            more_info(team)
+        end
+    end
+
 end
