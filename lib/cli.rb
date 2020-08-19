63dc0c6ef9b442ce4b23a8e31c62c8d816757f58
NickSpangler <Spangleyman@mac.com>
2020-08-19 16:00:46 -0400

    finished 'display_team' method

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,6 +5,8 @@ class CLI
     
     def run
         puts "Welcome to The Amazing Race!"
+        puts "The teams are meeting at the starting line."
+        puts "You'd better hurry if you want to join them!"
         CBS.big_bang
         puts "Are you ready to explore Season 13? Hit 'return' to learn more!"
         continue
@@ -28,71 +30,74 @@ class CLI
         puts "Finish line #{s.finish}"
         puts "Air dates: #{s.air_dates}"
         teams_or_episodes
-    end 
+    end
 
-        def teams_or_episodes
-            puts "For a list of teams to explore, enter 'Teams'."
-            puts "For a list of episodes to explore, enter 'Episodes'."
-            input = gets.strip.downcase
-            case input
-            when "teams"
-                list_teams
-            when "episodes"
-                list_episodes
-            else
-                teams_or_episodes
-            end
+    def display_team(number)
+        t = Team.all.find{ |team| team.finish == number }
+        puts ""
+        puts t.name
+        puts ""
+        puts "Relationship: #{t.relationship}"
+        puts "Hometown: #{t.hometown}"
+        puts "Occupation: #{t.occupation}"
+        puts "Finished: #{t.place}"
+        puts ""
+        puts t.about
+        puts ""
+        
+    end
+
+    def teams_or_episodes
+        puts "For a list of teams to explore, enter 'Teams'."
+        puts "For a list of episodes to explore, enter 'Episodes'."
+        input = gets.strip.downcase
+        case input
+        when "teams"
+            list_teams
+        when "episodes"
+            list_episodes
+        else
+            teams_or_episodes
         end
+    end
 
-        def list_teams
-            puts ""
-            Team.all.each.with_index(1){ |team, i| puts "#{i}. #{team.name}"}
-            puts ""
-            puts "Enter a team number to learn more about them."
-            puts "For a list of episodes to explore, enter 'Episodes'."
-            puts "To return to the season page, enter 'Season'."
-            input = gets.strip.downcase
-            if input.to_i.between?(1, 11)
-                puts "You entered team ##{input.to_i - 1}."
-            elsif input == "episodes"
-                list_episodes
-            elsif input == "season"
-                display_season
-            else
-                list_teams
-            end
+    def list_teams
+        puts ""
+        Team.all.each.with_index(1){ |team, i| puts "#{i}. #{team.name}"}
+        puts ""
+        puts "Enter a team number to learn more about them."
+        puts "For a list of episodes to explore, enter 'Episodes'."
+        puts "To return to the season page, enter 'Season'."
+        input = gets.strip.downcase
+        if input.to_i.between?(1, 11)
+            display_team(input.to_i)
+        elsif input == "episodes"
+            list_episodes
+        elsif input == "season"
+            display_season
+        else
+            list_teams
         end
+    end
 
-        def list_episodes
-            puts ""
-            Episode.all.each.with_index(1){ |episode, i| puts "#{i}. #{episode.title}"}
-            puts ""
-            puts "Enter an episode number to learn more about it."
-            puts "For a list of teams to explore, enter 'Teams'."
-            puts "To return to the season page, enter 'Season'."
-            input = gets.strip.downcase
-            if input.to_i.between?(1, 11)
-                display_team(input.to_i - 1)
-            elsif input == "teams"
-                list_teams
-            elsif input == "season"
-                display_season
-            else
-                list_episodes
-            end
-
-            def display_team(number)
-                t = Team.all.find{ |team| t.finish == number }
-                puts ""
-                puts t.name
-                puts ""
-                puts "Relationship: #{t.relationship}"
-                puts "Hometown: #{t.hometown}"
-                puts "Occupation: #{t.occupation}"
-                puts "Finished: #{t.place}"
-                
-            end
+    def list_episodes
+        puts ""
+        Episode.all.each.with_index(1){ |episode, i| puts "#{i}. #{episode.title}"}
+        puts ""
+        puts "Enter an episode number to learn more about it."
+        puts "For a list of teams to explore, enter 'Teams'."
+        puts "To return to the season page, enter 'Season'."
+        input = gets.strip.downcase
+        if input.to_i.between?(1, 11)
+            display_episode(input.to_i)
+        elsif input == "teams"
+            list_teams
+        elsif input == "season"
+            display_season
+        else
+            list_episodes
         end
+    end
     
     #   def choose_season
     #     puts "Please select a season of The Amazing Race to explore. You can type a number between 1 and 31."
