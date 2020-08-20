23a37744bcc8345a3b2a0ab0266ba1d0bfb666b3
NickSpangler <Spangleyman@mac.com>
2020-08-20 10:57:42 -0400

    adding 'exit_programming' functionality

--- lib/cli.rb
+++ lib/cli.rb
@@ -14,7 +14,7 @@ class CLI
     end
                                                                                                       
     def continue                                                                                                                                                                                                               
-        STDIN.getch                                                                                                              
+        STDIN.getch                                                                                                             
         print "            \r"                                                                                                             
     end 
 
@@ -144,6 +144,7 @@ class CLI
         puts "Enter an episode number to learn more about it."
         puts "For a list of teams to explore, enter 'Teams'."
         puts "To return to the season page, enter 'Season'."
+        puts "If you are finished, enter 'Exit'."
         input = gets.strip.downcase
         if input.to_i.between?(1, 11)
             display_episode(input.to_i)
@@ -151,6 +152,8 @@ class CLI
             list_teams
         elsif input == "season"
             display_season
+        elsif input == "exit"
+            exit_program
         else
             list_episodes
         end
@@ -166,13 +169,17 @@ class CLI
         puts ""
         puts "To choose another team, enter 'Teams'."
         puts "To choose an episode to explore, enter 'Episodes'."
+        puts "If you are finished, enter 'Exit'."
         input = gets.strip.downcase
-        if input == "teams"
+        case input
+        when "teams"
             list_teams
-        elsif input == "episodes"
+        when "episodes"
             list_episodes
+        when "exit"
+            exit_program
         else
-            more_info(team)
+            invalid_input
         end
     end
 
@@ -180,6 +187,29 @@ class CLI
         puts ""
         puts "Thank you for racing!"
         puts ""
+    end
+
+    def invalid_input
+        puts "I'm sorry, I'm not sure what you mean."
+        puts "For a list of teams, enter 'Teams'."
+        puts "For a list of episodes, enter 'Episodes'."
+        puts "If you are finished, enter 'Exit'."
+        input = gets.strip.downcase
+        case input
+        when "teams"
+            list_teams
+        when "episodes"
+            list_episodes
+        when "exit"
+            exit_program
+        else
+            invalid_input
+        end
+    end
+
+    def run_race(team)
+        puts ""
+        puts "Congratulations "
     end
 
 end
